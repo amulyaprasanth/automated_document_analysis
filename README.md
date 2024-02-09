17cfdb73c8a84b8347b5d7c8269f095afe6dedcb
Nadagani Amulya Prasanth <75437956+amulyaprasanth@users.noreply.github.com>
2024-02-09 19:25:16 +0530

    license and readme added

---
+++ README.md
@@ -0,0 +1,12 @@
+# Automated Document Analysis: OCR-based Human vs Machine Text Detection
+
+### Project workflows
+1. Update config.yaml
+2. Update params.yaml
+3. Update the entity
+4. Update configuration manager in src config
+5. Update the component
+6. Update the pipeline
+7. Update main.py
+8. Update dvc.yaml
+9. Update app.py
--- setup.py
+++ setup.py
@@ -6,3 +6,12 @@ AUTHOR_NAME = "amulyaprasanth"
 SRC_REPO = "automated_document_analysis"
 AUTHOR_EMAIL = "amulyaprasanth301@gmail.com"
 
+setuptools.setup(
+    name = SRC_REPO,
+    version = __version__,
+    author = AUTHOR_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "Automated Document Analysis: Human vs. Machine Text Detection",
+    url = f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}",
+    packages = setuptools.find_packages(where="src")
+)
