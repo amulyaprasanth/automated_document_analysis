a61eca06559fd0c753ced317a58a83659b7386a6
Nadagani Amulya Prasanth <75437956+amulyaprasanth@users.noreply.github.com>
2024-02-09 19:19:59 +0530

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,6 @@
+
+8
+setup.py,8\e\8e2edce0d507e1297474f25c00cae94258db38d8
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+__version__ = "0.0.0"
+REPO_NAME = "automated_document_analysis"
+AUTHOR_NAME = "amulyaprasanth"
+SRC_REPO = "automated_document_analysis"
+AUTHOR_EMAIL = "amulyaprasanth301@gmail.com"
+
